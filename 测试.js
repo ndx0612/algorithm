d04847cb59f9dd995e11281267035868d3dd9ffb
ndx2527 <1378362527@qq.com>
2021-11-05 09:33:45 +0800

    11.05

--- 测试.js
+++ 测试.js
@@ -19,19 +19,6 @@ c.right = g;
 b.left = d;
 b.right = e;
 
-function f1(rootList, target) {
-  if (rootList == null || rootList.length == 0) return false;
-  var childList = [];//当前层所有节点的子节点，都在这个list中，这样传入下一层级的时候，就可以遍历整个层级的节点。
-  for (var i = 0; i < rootList.length; i++) {
-    if (rootList[i] != null && rootList[i].value == target) {
-      return true;
-    } else {
-      childList.push(rootList[i].left);
-      childList.push(rootList[i].right);
-    }
-  }
-  return f1(childList, target);
-}
 
 
 console.log(f1([a], "d"));
