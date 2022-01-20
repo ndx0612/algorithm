e400855326eaabc8a747079f8387d32ca8a72d08
ndx2527 <1378362527@qq.com>
2022-01-20 10:28:08 +0800

    01.20哈希表、二分法复习

--- 02方法/01-二分法/01.二分查找.js
+++ 02方法/01-二分法/01.二分查找.js
@@ -15,5 +15,10 @@ var search = function (nums, target) {
   return -1;
 };
 
-let nums = [-1, 0, 3, 5, 9, 12], target = 9;
+let nums = [-1, 0, 3, 5, 9, 12], target = 5;
+// min1 max6 mid3  下标：0   5  -> 2
+// min4 max6 mid5  下标：3   5  -> 4
+// min4 max4 mid4  下标：3   3   -> 3
+
+
 console.log(search(nums, target));
--- 02方法/02-哈希表/01.两数之和.js
+++ 02方法/02-哈希表/01.两数之和.js
@@ -1,15 +1,15 @@
 // https://leetcode-cn.com/problems/two-sum/
 
-// 1.用map来存放{坐标，数值}这样的键值对
-// 2.运用逆向解法，即用target减去数组中的某个元素，然后来判断map中是否有相同的值，若有则存在满足条件的答案，返回两个坐标即可；若没有，则保存{数组中某个元素值，对应的坐标}到map对象中。依次遍历即可判断是否有满足条件的两个元素。
-
+// 1、判断map中是否有这个键(target-num[i])
+// 2、没有的话添加
+// 3、有的话返回
 var twoSum = function (nums, target) {
-  let map = new Map()
+  const mymap = new Map();
   for (let i = 0; i < nums.length; i++) {
-    if (map.has(target - nums[i])) {
-      return [map.get(target - nums[i]), i]
+    if (mymap.has(target - nums[i])) {
+      return [mymap.get(target - nums[i]), i]
     }
-    map.set(nums[i], i)
+    mymap.set(nums[i], i)
   }
 };
 
