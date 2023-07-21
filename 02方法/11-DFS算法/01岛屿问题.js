be6db9e7fea8b65df158e375817f530015fde8f1
ndx0612 <1378362527@qq.com>
2023-07-21 16:16:07 +0800

    feat: 岛屿问题-最大面积

---
+++ 02方法/11-DFS算法/01岛屿问题.js
@@ -0,0 +1,51 @@
+// 695. 岛屿的最大面积https://leetcode.cn/problems/max-area-of-island/submissions/
+const grid = [
+  ["1", "1", "1", "1", "0"],
+  ["1", "1", "0", "1", "0"],
+  ["1", "1", "0", "0", "0"],
+  ["0", "0", "0", "0", "0"]
+]
+
+// 岛屿问题
+const dfs = (grid, i, j) => {
+  // 边界直接return
+  if (!isArea(grid, i, j)) {
+    return 0;
+  }
+  // 不是陆地，直接return
+  if (grid[i][j] == 0 || grid[i][j] == 2) {
+    return 0;
+  }
+  // 将格子标记为「已遍历过」
+  grid[i][j] = 2;
+  // 继续前进
+  return 1 +
+    dfs(grid, i - 1, j) +
+    dfs(grid, i, j - 1) +
+    dfs(grid, i + 1, j) +
+    dfs(grid, i, j + 1);
+}
+
+// 判断是否在网络中，在网格中返回true，不在网格中返回false
+const isArea = (grid, i, j) => {
+  return 0 <= i && i < grid.length && 0 <= j && j < grid[0].length;
+}
+
+// 岛屿最大面积
+var maxAreaOfIsland = function (grid) {
+  let res = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] == 1) {
+        let s = dfs(grid, i, j)
+        if (res < s) {
+          res = s;
+        }
+      }
+    }
+  }
+  return res;
+};
+console.log(grid)
+console.log(maxAreaOfIsland(grid))
+console.log(grid)
